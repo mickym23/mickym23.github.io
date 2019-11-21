e177a50b3b460443026cdb03da48152f99746790
mickym23 <mikhail.cedras@gmail.com>
2019-11-21 23:19:57 +0200

    Weather Summary

--- lesson10/js/preston-api.js
+++ lesson10/js/preston-api.js
@@ -4,15 +4,46 @@ fetch(apiURL)
 
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
-    let obj = jsObject.main;
 
-    let high = obj.temp_max;
-    let humidity = obj.humidity;
+    let high = jsObject.main.temp_max;
+    let humidity = jsObject.main.humidity;
     let windSpeed = jsObject.wind.speed;
 
-    document.getElementById('highTemp').textContent = high;
-    document.getElementById('humidity').textContent = humidity;
-    document.getElementById('wspeed').textContent = windSpeed;
+   let hTemp = document.querySelector('#temp');
+
+   if (hTemp) {
+      hTemp.textContent = high;
+   }
+
+   let hum = document.querySelector('#humidity');
+
+   if (hum) {
+      hum.textContent = humidity + '%';
+   }
+
+   let speed = document.querySelector('#wspeed');
+
+   if (speed) {
+      speed.textContent = windSpeed;
+   }
+
+
+   function windchill (high, windSpeed)  {
+
+      // IF-ELSE statement to determine the Wind Chill
+      if( windSpeed > 3 && high <= 50) {
+      
+         var windchill = Math.round(35.74 + (0.6215 * high) - (35.75 * (Math.pow(windSpeed, 0.16))) + (0.4275 * high * (Math.pow(windSpeed, 0.16))));
+      
+         document.getElementById("windchill").innerHTML = windchill + "&#8457;";
+
+      }  else {
+         
+         document.getElementById("windchill").innerHTML = "N/A"; 
+      
+      }
+   }
+
+   windchill(high, windSpeed);
 
   });
