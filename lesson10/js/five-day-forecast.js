b44320b5d61664c2355807bf827de525d227a6be
mickym23 <mikhail.cedras@gmail.com>
2019-11-23 21:35:19 +0200

    Fixed Forecast

---
+++ lesson10/js/five-day-forecast.js
@@ -0,0 +1,60 @@
+// API.OPENWEATHERMAP.ORG Forecast JSON URL
+const apiURLForecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=200cf102e64ee34534ad4bd4429e42bc";
+
+// Fetch API Forecast URL
+fetch(apiURLForecast)
+
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+        // Iterate through the JSON for all 5 Forecast days 
+        for (let i = 0; i < jsObject.list.length; i++) {
+
+            // IF statement that checks JSON values for 18:00:00
+            if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+
+                // Setting card, name of day, image, and paragraphs
+                let card = document.createElement('section');
+                let day = document.createElement('h3');
+                let image = document.createElement('img');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+
+                // Retrieving the icon 'code' and URL for current weather image SRC
+                let src = 'https://openweathermap.org/img/wn/' + jsObject.list[i].weather[0].icon + '@2x.png';
+
+                // Retrieving weather description for image ALT
+                let alt = jsObject.list[i].weather[0].description;
+
+                // Set day name array and current date
+                let dayArr = ["SUN","MON","TUES","WED","THURS","FRI","SAT"];
+                let datum = new Date(jsObject.list[i].dt_txt);
+                
+                /*
+                 * Setting:
+                 *          Name of Day
+                 *          Temperature (Current)
+                 *          Wind Speed
+                 *          Humidity Level
+                 *          Image SRC + ALT
+                 */
+                day.textContent = dayArr[datum.getDay()];
+                p1.innerHTML = "Temperature: " +  jsObject.list[i].main.temp + '&#8457;';
+                p2.innerHTML = "Wind Speed: " + jsObject.list[i].wind.speed + " mph";
+                p3.innerHTML = 'Humidity: ' +  jsObject.list[i].main.humidity + "%";
+                image.setAttribute('src', src);
+                image.setAttribute('alt', alt);
+                
+                // Appending the new variables to the section card
+                card.appendChild(day);
+                card.appendChild(image);
+                card.appendChild(p1);
+                card.appendChild(p2);
+                card.appendChild(p3);
+
+                // Appending card to 'div.forecast' in HTML
+                document.querySelector('div.forecast').appendChild(card);
+            }
+        }
+    });
--- lesson10/js/preston-api.js
+++ lesson10/js/preston-api.js
@@ -1,39 +1,58 @@
+// API.OPENWEATHER.ORG Today's JSON Data URL
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=200cf102e64ee34534ad4bd4429e42bc"
 
+// Fetch URL
 fetch(apiURL)
 
-  .then((response) => response.json())
-  .then((jsObject) => {
+   // 
+   .then((response) => response.json())
+   .then((jsObject) => {
 
-    let high = jsObject.main.temp_max;
-    let humidity = jsObject.main.humidity;
-    let windSpeed = jsObject.wind.speed;
+      // Set variable to JSON data values
+      let high = jsObject.main.temp_max;
+      let humidity = jsObject.main.humidity;
+      let windSpeed = jsObject.wind.speed;
+      let currentTemp = jsObject.main.temp;
 
-   let hTemp = document.querySelector('#temp');
+      // Setting variable to ID of same name
+      let cTemp = document.querySelector('#cTemp');
 
-   if (hTemp) {
-      hTemp.textContent = high;
-   }
+      // Setting JSON value to variable + ID
+      if (cTemp) {
+         cTemp.textContent = currentTemp;
+      }
 
-   let hum = document.querySelector('#humidity');
+      // Setting variable to ID of same name
+      let hTemp = document.querySelector('#hTemp');
 
-   if (hum) {
-      hum.textContent = humidity + '%';
-   }
+      // Setting JSON value to variable + ID
+      if (hTemp) {
+         hTemp.textContent = high;
+      }
 
-   let speed = document.querySelector('#wspeed');
+      // Setting variable to ID of same name
+      let hum = document.querySelector('#humidity');
 
-   if (speed) {
-      speed.textContent = windSpeed;
-   }
+      // Setting JSON value to variable + ID
+      if (hum) {
+         hum.textContent = humidity + '%';
+      }
 
+      // Setting variable to ID of same name
+      let speed = document.querySelector('#wspeed');
 
-   function windchill (high, windSpeed)  {
+      // Setting JSON value to variable + ID
+      if (speed) {
+         speed.textContent = windSpeed;
+      }
+
+      // Revised Windchill Function including current temperature and wind speed
+      function windchill (currentTemp, windSpeed)  {
 
       // IF-ELSE statement to determine the Wind Chill
-      if( windSpeed > 3 && high <= 50) {
+      if( windSpeed > 3 && currentTemp <= 50) {
       
-         var windchill = Math.round(35.74 + (0.6215 * high) - (35.75 * (Math.pow(windSpeed, 0.16))) + (0.4275 * high * (Math.pow(windSpeed, 0.16))));
+         let windchill = Math.round(35.74 + (0.6215 * currentTemp) - (35.75 * (Math.pow(windSpeed, 0.16))) + (0.4275 * currentTemp * (Math.pow(windSpeed, 0.16))));
       
          document.getElementById("windchill").innerHTML = windchill + "&#8457;";
 
@@ -42,8 +61,8 @@ fetch(apiURL)
          document.getElementById("windchill").innerHTML = "N/A"; 
       
       }
-   }
+      }
 
-   windchill(high, windSpeed);
-
+      // Calling windchill funciton with appropiate parameters
+      windchill(currentTemp, windSpeed);
   });
