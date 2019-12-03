89026b4513479377f6b57f9f56bde0007e6ccf7f
mickym23 <mikhail.cedras@gmail.com>
2019-12-03 08:38:41 +0200

    Removed console.log

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -3,7 +3,7 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject) => {
-  console.log(jsObject);
+
   document.getElementById('current-temp').textContent = jsObject.main.temp;
 
   // Concatenate the extension
