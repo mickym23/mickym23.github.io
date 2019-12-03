2b75b63c39de1119eed0519749e2fe7cf2d0923f
mickym23 <mikhail.cedras@gmail.com>
2019-12-03 05:44:42 +0200

    Added new pages

--- lesson11/js/townevents.js
+++ lesson11/js/townevents.js
@@ -9,45 +9,40 @@ fetch (requestURL)
       // Access 'towns' JSON array
       const arr = jsonObject['towns'];
 
-      // Run a FOR loop through each array  to create all three cards
+/************************************************************ Preston ********************************************************/
       for (let i = 0; i < arr.length; i++) {
-          // IF statement to loop through only Fish Haven, Preston, and Soda Springs town names 
-          if (arr[i].name === "Fish Haven" || arr[i].name === "Preston" || arr[i].name === "Soda Springs") {  
+
+            // Calling for Preston JSON data
+          if (arr[i].name === "Preston") {  
           
           // Creating different HTML tag variables
           let card = document.createElement('section');
           let h1 = document.createElement('h1');
-          let h2 = document.createElement('h2');
           let p1 = document.createElement('p');
           let p2 = document.createElement('p');
           let p3 = document.createElement('p');
-
-          let img = document.createElement('img'); 
           let text = document.createElement('div');
+          let linebreak = document.createElement('br'); 
+          let linedash = document.createElement('hr');
 
           // Storing JSON data in HTML tag variables
-          h1.textContent = arr[i].name;
-          h2.textContent = arr[i].motto;
-          p1.textContent = "Year Founded: " + arr[i].yearFounded;
-          p2.textContent ="Current Population: " + arr[i].currentPopulation;
-          p3.textContent ="Average Rainfall: " + arr[i].averageRainfall;
-        
-          // Setting the SRC and ALT for images (locally stored)
-          img.setAttribute('src', 'images/'+ arr[i].photo);
-          img.setAttribute('alt', "Iconic Image of " + arr[i].name);
-
+          h1.textContent = "Upcoming Events";
+          p1.textContent = arr[i].events[0];
+          p2.textContent = arr[i].events[1];
+          p3.textContent = arr[i].events[2];  
           
           // Creating entire content text
           text.appendChild(h1);
-          text.appendChild(h2);
+          text.appendChild(linebreak);
+          text.appendChild(linedash);
           text.appendChild(p1);
           text.appendChild(p2);
           text.appendChild(p3);
-
+         
           // Adding to section element
           card.appendChild(text);
-          card.appendChild(img);
 
           // Apply to .cards class 
           document.querySelector('div.cards').appendChild(card);
           }}});
+
