47319b86f2cda7b400ef62930e6735a329cc7689
mickym23 <mikhail.cedras@gmail.com>
2019-12-21 23:45:04 +0200

    Registration

--- lesson13/js/thankyou.js
+++ lesson13/js/thankyou.js
@@ -2,3 +2,9 @@ function messageUser() {
    var thanks = "Thank you for submitting! We'll get back to you shortly.";
    alert(thanks);
 }
+
+
+function thankYou() {
+   var thanks = "Thank you for your booking request. This will be processed shortly. An confirmation email will be sent to your address.";
+   alert(thanks);
+}
