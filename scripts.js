d799a06f1a402e4a2fc8f510cb97309c79552f74
mickym23 <mikhail.cedras@gmail.com>
2022-01-06 11:48:48 +0200

    Working on header inside index

---
+++ scripts.js
@@ -0,0 +1,45 @@
+let i = 0;
+let j = 0;
+let text = 'I am Mikhail Cedras';
+let text2 = "Junior Full Stack Web Developer";
+let speed = 200;
+
+function typeWriter2() {
+
+   if (j < text2.length) {
+      document.getElementById("headText2").innerHTML += text2.charAt(j);
+      j++;
+      setTimeout(() => {
+         typeWriter2();
+      }, 100);
+   }
+}
+
+function typeWriter() {
+   return new Promise((resolve, reject) => {
+      if (i < text.length) {
+         document.getElementById("headText1").innerHTML += text.charAt(i);
+         i++;
+
+         if (i == text.length) {
+            return;
+         }
+
+         setTimeout(() => {
+            typeWriter();
+            if (i == (text.length)) {
+               typeWriter2();
+               resolve();
+            }
+         }, speed);
+      }
+   })
+}
+
+async function render() {
+   await typeWriter();    
+}
+
+   window.addEventListener("DOMContentLoaded", () => {
+      render();
+   });
