31c2b3735eeb1c2f583ef55aa6882c66d6a83f6b
mickym23 <mikhail.cedras@gmail.com>
2022-01-11 19:51:38 +0200

    Added Construction page

--- scripts.js
+++ scripts.js
@@ -1,45 +1,45 @@
-let i = 0;
-let j = 0;
-let text = 'I am Mikhail Cedras';
-let text2 = "Junior Full Stack Web Developer";
-let speed = 200;
+// let i = 0;
+// let j = 0;
+// let text = 'I am Mikhail Cedras';
+// let text2 = "Junior Full Stack Web Developer";
+// let speed = 200;
 
-function typeWriter2() {
+// function typeWriter2() {
 
-   if (j < text2.length) {
-      document.getElementById("headText2").innerHTML += text2.charAt(j);
-      j++;
-      setTimeout(() => {
-         typeWriter2();
-      }, 100);
-   }
-}
+//    if (j < text2.length) {
+//       document.getElementById("headText2").innerHTML += text2.charAt(j);
+//       j++;
+//       setTimeout(() => {
+//          typeWriter2();
+//       }, 100);
+//    }
+// }
 
-function typeWriter() {
-   return new Promise((resolve, reject) => {
-      if (i < text.length) {
-         document.getElementById("headText1").innerHTML += text.charAt(i);
-         i++;
+// function typeWriter() {
+//    return new Promise((resolve, reject) => {
+//       if (i < text.length) {
+//          document.getElementById("headText1").innerHTML += text.charAt(i);
+//          i++;
 
-         if (i == text.length) {
-            return;
-         }
+//          if (i == text.length) {
+//             return;
+//          }
 
-         setTimeout(() => {
-            typeWriter();
-            if (i == (text.length)) {
-               typeWriter2();
-               resolve();
-            }
-         }, speed);
-      }
-   })
-}
+//          setTimeout(() => {
+//             typeWriter();
+//             if (i == (text.length)) {
+//                typeWriter2();
+//                resolve();
+//             }
+//          }, speed);
+//       }
+//    })
+// }
 
-async function render() {
-   await typeWriter();    
-}
+// async function render() {
+//    await typeWriter();    
+// }
 
-   window.addEventListener("DOMContentLoaded", () => {
-      render();
-   });
+//    window.addEventListener("DOMContentLoaded", () => {
+//       render();
+//    });
